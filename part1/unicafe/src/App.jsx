3c51e8c573fca989073cba31dc199b5852293f47
Poorvi <poorvi.b26@gmail.com>
2025-01-19 13:11:16 -0800

    complete part1 unicafe exercise

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / total;
+  const percentPositive = (good / total) * 100;
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>good</td>
+          <td>{good}</td>
+        </tr>
+        <tr>
+          <td>neutral</td>
+          <td>{neutral}</td>
+        </tr>
+        <tr>
+          <td>bad</td>
+          <td>{bad}</td>
+        </tr>
+        <tr>
+          <td>total</td>
+          <td>{total}</td>
+        </tr>
+        <tr>
+          <td>average</td>
+          <td>{average.toFixed(1)}</td>
+        </tr>
+        <tr>
+          <td>postive</td>
+          <td>{percentPositive.toFixed(1)}%</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGood = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBad = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <Button handleClick={handleGood} text={"good"} />
+      <Button handleClick={handleNeutral} text={"neutral"} />
+      <Button handleClick={handleBad} text={"bad"} />
+      <h2>statistics</h2>
+      {good + bad + neutral === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <div>
+          <Statistics good={good} neutral={neutral} bad={bad} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;

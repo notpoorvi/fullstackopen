b43e368f24c874b6fa43f98d7514ca8eec2f2dca
Poorvi <poorvi.b26@gmail.com>
2025-02-05 09:39:46 -0800

    complete phonebook step4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchVal, setSearchVal] = useState("");
 
   const addNameNumber = (event) => {
     event.preventDefault();
@@ -32,9 +36,16 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleSearchValChange = (event) => {
+    setSearchVal(event.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <span>filter shown with </span>
+      <input type="text" value={searchVal} onChange={handleSearchValChange} />
+      <h2>add a new</h2>
       <form onSubmit={addNameNumber}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -47,11 +58,32 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <div key={person.name}>
-          {person.name} {person.number}
-        </div>
-      ))}
+      {persons
+        .filter(
+          (person) =>
+            searchVal === "" ||
+            person.name.toLowerCase().includes(searchVal.toLowerCase())
+        )
+        .map((person) => (
+          <div key={person.name}>
+            {person.name} {person.number}
+          </div>
+        ))}
+      {searchVal === ""
+        ? persons.map((person) => (
+            <div key={person.name}>
+              {person.name} {person.number}
+            </div>
+          ))
+        : persons
+            .filter((person) =>
+              person.name.toLowerCase().includes(searchVal.toLowerCase())
+            )
+            .map((person) => (
+              <div key={person.name}>
+                {person.name} {person.number}
+              </div>
+            ))}
     </div>
   );
 };

1c2e91d35d3a970b4ee474f339b2a1de9880ce71
Poorvi <poorvi.b26@gmail.com>
2025-02-07 12:46:32 -0800

    modified phonebook (part 2) to get data from db.json using json-server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Filter = ({ searchVal, handleSearchValChange }) => {
   return (
@@ -52,15 +53,19 @@ const Persons = ({ persons, searchVal }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchVal, setSearchVal] = useState("");
+
+  const hook = () => {
+    axios.get("http://localhost:3001/persons").then((respone) => {
+      const data = respone.data;
+      setPersons(data);
+    });
+  };
+
+  useEffect(hook, []);
 
   const addNameNumber = (event) => {
     event.preventDefault();

7eabf94427e8f7351c6e3aea6d78dead3bdfde49
Poorvi <poorvi.b26@gmail.com>
2025-02-25 20:56:15 -0800

    add error handler middleware to notes app and code the put method

--- notes-app/backend/index.js
+++ notes-app/backend/index.js
@@ -1,9 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-const Note = require('./models/note')
 const app = express()
-app.use(express.static('dist'))
-app.use(express.json())
 
 const requestLogger = (request, response, next) => {
     console.log('Method: ', request.method)
@@ -12,7 +9,11 @@ const requestLogger = (request, response, next) => {
     console.log('---')
     next()
 }
+
+app.use(express.static('dist'))
+app.use(express.json())
 app.use(requestLogger)
+const Note = require('./models/note')
 
 const cors = require('cors')
 app.use(cors())
@@ -28,34 +29,38 @@ app.get('/api/notes', (request, response) => {
     })
 })
 // handle HTTP GET requests made to a particular note from application
-app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => response.json(note))
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id).then(note => {
+        if (note) {
+            response.json(note)
+        }
+        else {
+            response.status(404).send({ error: "note not found" }).end()
+        }
+    }).catch(error => {
+        next(error)
+    })
 })
-
 // handle delete requests
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => Number(note.id) !== id)
-
-    response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id).then(result => {
+        if (result) {
+            response.status(204).end()
+        } else {
+            response.status(400).send({ error: "note does not exist" })
+        }
+    }).catch(error => next(error))
 })
-
-const generateID = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-}
 
 app.put('/api/notes/:id', (request, response) => {
-    const id = request.params.id; const body = request.body;
-    const note = notes.find(n => n.id === id);
-    if (!note) {
-        return response.status(404).json({ error: "Note not found" });
-    }
-    const updatedNote = { ...note, important: body.important };
-    notes = notes.map(n => (n.id === id ? updatedNote : n));
-    response.json(updatedNote);
+    const body = request.body;
+    const note = {
+        content: body.content,
+        important: body.important
+    };
+    Note.findByIdAndUpdate(request.params.id, note, { new: true }).then(updatedNote => {
+        response.json(updatedNote)
+    }).catch(error => next(error))
 });
 
 app.post('/api/notes/', (request, response) => {
@@ -76,6 +81,15 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+    if (error.name == 'CastError') {
+        return response.status(400).send({ error: "malformatted id" })
+    }
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
